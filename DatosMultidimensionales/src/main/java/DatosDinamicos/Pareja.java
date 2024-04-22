0c8540e5aeaab7cfb740f579990a264240eaf9a7
KravAD <dkrav@myuax.com>
2024-04-22 17:50:22 +0200

    commit 3
    Atributos de entero primero y segundo + constructor de la clase.

--- DatosMultidimensionales/src/main/java/DatosDinamicos/Pareja.java
+++ DatosMultidimensionales/src/main/java/DatosDinamicos/Pareja.java
@@ -1,4 +1,11 @@
 package DatosDinamicos;
 
 public class Pareja {
+    private int primero;
+    private int segundo;
+
+    public Pareja(int primero, int segundo) {
+        this.primero = primero;
+        this.segundo = segundo;
+    }
 }

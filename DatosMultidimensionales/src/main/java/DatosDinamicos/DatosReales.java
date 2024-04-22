988d571696028cd1cae7fa444a8c354a8be437e3
KravAD <dkrav@myuax.com>
2024-04-22 17:58:09 +0200

    commit 6
    Constructor de la clase.

--- DatosMultidimensionales/src/main/java/DatosDinamicos/DatosReales.java
+++ DatosMultidimensionales/src/main/java/DatosDinamicos/DatosReales.java
@@ -1,8 +1,13 @@
 package DatosDinamicos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatosReales {
     private List<Double> datos;
+    
+    public DatosReales() {
+        this.datos = new ArrayList<>();
+    }
 
 }

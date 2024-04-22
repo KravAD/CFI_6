cfb9e21512370815f68b31b0054a174f61990908
KravAD <dkrav@myuax.com>
2024-04-22 18:05:20 +0200

    commit 9
    Atributo de lista de pares y constructor de la clase.

--- DatosMultidimensionales/src/main/java/DatosDinamicos/DatosPares.java
+++ DatosMultidimensionales/src/main/java/DatosDinamicos/DatosPares.java
@@ -1,4 +1,12 @@
 package DatosDinamicos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatosPares {
+    private List<Pareja> pares;
+
+    public DatosPares() {
+        this.pares = new ArrayList<>();
+    }
 }

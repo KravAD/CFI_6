cdcaa754251a7a0bb4b85fc5ff29e50279f98c35
KravAD <dkrav@myuax.com>
2024-04-22 18:02:13 +0200

    commit 7
    Métodos para agregar, obtener y eliminar datos.

--- DatosMultidimensionales/src/main/java/DatosDinamicos/DatosReales.java
+++ DatosMultidimensionales/src/main/java/DatosDinamicos/DatosReales.java
@@ -5,9 +5,18 @@ import java.util.List;
 
 public class DatosReales {
     private List<Double> datos;
-    
+
     public DatosReales() {
         this.datos = new ArrayList<>();
+    }
+    public void agregarDato (double dato) {
+        this.datos.add(dato);
+    }
+    public double obtenerDato (int posicion) {
+        return this.datos.get(posicion);
+    }
+    public void eliminarDato (int posicion) {
+        this.datos.remove(posicion);
     }
 
 }

3667a76e78847d41971af15532ffe066d883a892
KravAD <dkrav@myuax.com>
2024-04-22 17:52:17 +0200

    commit 4
    Getters y setters para primero y segundo.

--- DatosMultidimensionales/src/main/java/DatosDinamicos/Pareja.java
+++ DatosMultidimensionales/src/main/java/DatosDinamicos/Pareja.java
@@ -8,4 +8,20 @@ public class Pareja {
         this.primero = primero;
         this.segundo = segundo;
     }
+
+    public int getPrimero() {
+        return primero;
+    }
+
+    public void setPrimero(int primero) {
+        this.primero = primero;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
+    }
 }

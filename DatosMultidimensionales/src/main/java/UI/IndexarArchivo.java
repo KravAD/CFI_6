26a0c7597e004ac9d2fe0b7ea00cafc3695b70f5
KravAD <dkrav@myuax.com>
2024-04-23 20:44:48 +0200

    commit 22
    Creo la clase IndexarArchivo.  El método indexarArchivo recorre un directorio y sus subdirectorios de manera recursiva. Guarda los archivos en el treemap y las claves son los nombres de los archivos. El método obtenerArchivos usa el StringBuilder para crear una lista de los archivos junto a su ruda de directorio.
    En interfaz de usuario añado un JButton que al presionarlo se indexan los archivos y se muetra la lista de ellos con la ruta.

---
+++ DatosMultidimensionales/src/main/java/UI/IndexarArchivo.java
@@ -0,0 +1,29 @@
+package UI;
+import java.io.File;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class IndexarArchivo {
+    private Map<String, String> indiceArchivos = new TreeMap<>();
+
+    public void indexarArchivos(File directorio) {
+        File[] archivos = directorio.listFiles();
+        if (archivos != null) {
+            for (File archivo : archivos) {
+                if (archivo.isDirectory()) {
+                    indexarArchivos(archivo);
+                } else {
+                    indiceArchivos.put(archivo.getName(), archivo.getAbsolutePath());
+                }
+            }
+        }
+    }
+
+    public String obtenerListadoArchivos() {
+        StringBuilder listado = new StringBuilder();
+        for (Map.Entry<String, String> entrada : indiceArchivos.entrySet()) {
+            listado.append("Nombre del archivo: ").append(entrada.getKey()).append(", Ruta: ").append(entrada.getValue()).append("\n");
+        }
+        return listado.toString();
+    }
+}
--- DatosMultidimensionales/src/main/java/UI/Interfaz.java
+++ DatosMultidimensionales/src/main/java/UI/Interfaz.java
@@ -2,6 +2,7 @@ package UI;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class Interfaz extends JFrame{
     public Interfaz(){
@@ -19,7 +20,23 @@ public class Interfaz extends JFrame{
             }
         });
 
+        JButton botonIndexarArchivos = new JButton("Indexar Archivos");
+        botonIndexarArchivos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                IndexarArchivo indexador = new IndexarArchivo();
+                indexador.indexarArchivos(new File("ruta/del/directorio"));
+                String listadoArchivos = indexador.obtenerListadoArchivos();
+
+                JTextArea areaTexto = new JTextArea(listadoArchivos);
+                JScrollPane scrollPane = new JScrollPane(areaTexto);
+                JOptionPane.showMessageDialog(null, scrollPane, "Listado de Archivos", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+
         panel.add(botonDatosDinamicos);
+        panel.add(botonIndexarArchivos);
         add(panel);
 
         pack();

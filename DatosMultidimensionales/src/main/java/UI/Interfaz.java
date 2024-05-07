e36667c9244c4d15deb8d19c5bb66ade19de0d5e
KravAD <dkrav@myuax.com>
2024-05-07 18:35:41 +0200

    commit 28
    Imagen de fondo.

--- DatosMultidimensionales/src/main/java/UI/Interfaz.java
+++ DatosMultidimensionales/src/main/java/UI/Interfaz.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Interfaz extends JFrame {
     private IndexarArchivo indexador = new IndexarArchivo();
@@ -17,7 +19,23 @@ public class Interfaz extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel() {
+            private Image backgroundImage;
+
+            {
+                try {
+                    backgroundImage = ImageIO.read(getClass().getResource("/uax.jpeg"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+            }
+        };
 
         JButton botonDatosDinamicos = new JButton("Datos Dinámicos");
         botonDatosDinamicos.setFont(new Font("Arial", Font.BOLD, 14));
@@ -29,7 +47,6 @@ public class Interfaz extends JFrame {
                 new VentanaDatosDinamicos().setVisible(true);
             }
         });
-
 
         JButton botonIndexarArchivos = new JButton("Indexar Archivos");
         botonIndexarArchivos.setFont(new Font("Arial", Font.BOLD, 14));
